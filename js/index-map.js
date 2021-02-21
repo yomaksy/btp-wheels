01a96f9d7d5d1ebe17b7abed1b4db77c5c8dfffb
Oleksii Maksymchuk <in4maks@gmail.com>
2021-02-21 16:36:19 +0200

    Updated

---
+++ js/index-map.js
@@ -0,0 +1,152 @@
+function initMap() {
+  const boutiqueLatLng = {lat: 48.42893141646616, lng: 35.03940286217705};
+  const boutiqueMap = new google.maps.Map(document.getElementById('boutique-map'), {
+    disableDefaultUI: true,
+    zoom: 16,
+    center: boutiqueLatLng,
+    styles: [
+      {
+          "featureType": "administrative",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.province",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 65
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": "50"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": "-100"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "30"
+              }
+          ]
+      },
+      {
+          "featureType": "road.local",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "lightness": "40"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "hue": "#ffff00"
+              },
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -97
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "lightness": -25
+              },
+              {
+                  "saturation": -100
+              }
+          ]
+      }
+  ],
+  });
+  const svgMarker = {
+    path: "M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z",
+    fillColor: "black",
+    fillOpacity: 1,
+    scale: 2,
+    anchor: new google.maps.Point(5, 18),
+  };
+  const customMarker = new google.maps.Marker({
+    position: boutiqueLatLng,
+    icon: svgMarker,
+    map: boutiqueMap,
+    title: "We are Here",
+  });
+  const info = new google.maps.InfoWindow({
+    content: '<h6>BTP Wheels</h6><p>105b Haharina Ave, Dnipro</p>'
+  });
+  customMarker.addListener('click', function() {
+    info.open(boutiqueMap, customMarker);
+  });
+}
