802be08b52547d0b534cf8a9394d758581637906
Oleksii Maksymchuk <in4maks@gmail.com>
2021-04-19 20:00:37 +0300

    Performed

--- js/plus.js
+++ js/plus.js
@@ -1,3 +1,12 @@
 function scrollDown() {
     window.scrollBy (0, window.innerHeight);
 };
+
+let enableChromeContrast = () => {
+    const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    if (isChrome) {
+        document.querySelector('body').style['image-rendering'] = '-webkit-optimize-contrast';
+    }
+};
+
+enableChromeContrast();
